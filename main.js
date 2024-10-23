cd49c401e6b179759143ad622ac80e99ef1be3cb
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-23 17:22:51 +0200

    add 1 funzione con condizione

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-console.log ("Biglietto del treno")
+console.log("Biglietto del treno")
+
+const btnGenera = document.querySelector('button')
+btnGenerate.addEventListener("click",
+    
+  function () {
+  // nome utente
+  let nameUser = document.getElementById("inputName").value
+  console.log(nameUser)
+
+  // età
+  let kmUser = document.getElementById("inputKm").value
+  console.log(kmUser)
+
+  //scelta 
+  let ageUser = document.getElementById("inputSconto").value
+  console.log(ageUser)
+
+  const prezzo_al_km = 0.21
+
+  //condizione 
+  if (ageUser < 18) {
+    const prezzo_scontato20 = (kmUser * prezzo_al_km) * 20 / 100
+    let prezzo_finale = (km_viaggio * prezzo_al_km) - prezzo_scontato20
+    console.log(`Il prezzo totale con lo sconto del 20% è: ${prezzo_finale.toFixed(2)}€`)
+
+  } else if (ageUser >= 65) {
+    const prezzo_scontato40 = (kmUser * prezzo_al_km) * 40 / 100
+    let prezzo_finale = (km_viaggio * prezzo_al_km) - prezzo_scontato40
+    console.log(`Il prezzo totale con lo sconto del 40% è: ${prezzo_finale.toFixed(2)}€`)
+  }
+
+  else {
+    let prezzo_pieno = kmUser * prezzo_al_km
+    console.log(`Il prezzo totale è: ${prezzo_pieno.toFixed(2)}€`)
+  }
+
+
+  
+  document.getElementById('nome-passegero').innerHTML = inputName
+
+})
